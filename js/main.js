50d4d53913921d210af54e85c0460e13abcc83ad
tsumiki4102 <72963186+tsumiki4102@users.noreply.github.com>
2021-05-03 14:12:27 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -56,4 +56,22 @@
     open.classList.remove('hide');
     close.classList.remove('show');
   });
+
+  //スムーススクロール
+  const smoothScrollTrigger = document.querySelectorAll('a[href^="#"]');
+  for (let i = 0; i < smoothScrollTrigger.length; i++){
+    smoothScrollTrigger[i].addEventListener('click', (e) => {
+      e.preventDefault();
+      let href = smoothScrollTrigger[i].getAttribute('href');
+      let targetElement = document.getElementById(href.replace('#', ''));
+      const rect = targetElement.getBoundingClientRect().top;
+      const offset = window.pageYOffset;
+      const gap = 60;
+      const target = rect + offset - gap;
+      window.scrollTo({
+        top: target,
+        behavior: 'smooth',
+      });
+    });
+  }
 }
